111eb411b6880d6098471b8cbaf143cf9334ec4c
cash <cash@cashwu.com>
2017-12-09 09:42:03 +0800

    add test proj

---
+++ Sample.ServiceTests/SchoolInfoServiceTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Sample.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample.Service.Tests
+{
+    [TestClass()]
+    public class SchoolInfoServiceTests
+    {
+        [TestMethod()]
+        public void GetAllSchoolInfosTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
